ea19513be5738864ccf58aef6d4c35765c8cb68a
David Kinzer <dtkinzer@gmail.com>
2013-04-27 02:10:37 -0400

    Initial commit.
    Everything is centered.

---
+++ etc/center.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Keep things horizontally centered.
+ */
+
+jQuery.fn.center = function() {
+    this.css({
+        'position': 'fixed',
+        'left': '50%',
+    });
+    this.css({
+        'margin-left': -this.width() / 2 + 'px',
+    });
+
+    return this;
+}
+
+
+$(window).resize(function(){
+ $('img#back-ground').center();
+});
+
+$('img#back-ground').center();

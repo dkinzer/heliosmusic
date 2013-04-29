d183b9425092e7d90509d2575231b6803aa5f897
David Kinzer <dtkinzer@gmail.com>
2013-04-29 01:52:54 -0400

    Fix outoing email.

--- lib/contact/contact.php
+++ lib/contact/contact.php
@@ -1,13 +1,13 @@
 <?php
 header('Content-type: application/json');
 
-if (isset($_POST['element_999'])&& $_POST['element_999'] != '') {
+if (isset($_POST['element_999']) && $_POST['element_999'] == '') {
 
   foreach ($_POST as $key => $value) {
     $$key = htmlspecialchars($value);
   }
 
-  $to = "dtkinzer@gmail.com";
+  $to = "info@heliosmusic.com";
   $subject = "Message from helios contact form.";
 
   $message = "

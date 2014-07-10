a4b0a3391529e092ee831a05f8baf0d5038ec11e
David Kinzer <dtkinzer@gmail.com>
2014-07-10 12:01:19 -0400

    Dont prefix.
    It's adding too much complication.

--- bin/archive.sh
+++ bin/archive.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/sh
 
-if [ "$(uname)" == "Darwin"];
-then
-  PREFIX=-s /^\./www/g
-else
-  PREFIX=--transform 's,^\.,www,'
-fi
-
 touch heliosmusic.tgz
 tar --exclude='.git*' \
   --exclude='.sass-cache*' \
@@ -16,4 +9,4 @@ tar --exclude='.git*' \
   --exclude='Gemfile*' \
   --exclude='Makefile' \
   --exclude='heliosmusic.tgz' \
-  $PREFIX -czvf heliosmusic.tgz .
+  -czvf heliosmusic.tgz .

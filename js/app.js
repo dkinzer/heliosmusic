feba45721906874988e3f02d49a80460a742553f
David Kinzer <dtkinzer@gmail.com>
2014-07-11 18:13:39 -0400

    Add back ajax handler for contact form.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('.results').hide();
+$('#contactFormModal').submit(function(e){
+  e.preventDefault();
+  $.ajax({
+    type : 'POST',
+    cache : false,
+    url : "lib/contact.php",
+    data : 'id=contact_form'+$(this).serialize(),
+    success : function(msg) {
+      $('#contact-form-error').hide();
+      $('#contac-form-thanks').show();
+      console.log(msg);
+    },
+    error : function(error) {
+      $('#form-thanks').hide();
+      $('#form-error').show();
+      console.log(error);
+    }
+  });
+  return false;
+});

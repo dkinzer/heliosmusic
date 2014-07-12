aa2c2a53db02d846991bff37ad3a99bf9af14e59
David Kinzer <dtkinzer@gmail.com>
2014-07-12 15:01:29 -0400

    Dynamically resize itunes iframe width.

--- js/app.js
+++ js/app.js
@@ -23,3 +23,19 @@ $('#contact-form').submit(function(e){
   });
   return false;
 });
+
+
+$(function(){
+  var width = (function(){
+    if (matchMedia(Foundation.media_queries.large).matches) {
+      return 500;
+    } else if (matchMedia(Foundation.media_queries.medium).matches) {
+      return 350;
+    }
+    else {
+      return 200;
+    }})();
+
+    var src = "http://widgets.itunes.apple.com/widget.html?c=us&brc=FFFFFF&blc=FFFFFF&trc=FFFFFF&tlc=FFFFFFd=t=&m=music&e=album&w=" + width + "&h=395&ids=535362219&wt=discovery&partnerId=&affiliate_id=";
+    return $('iframe#itunes').attr('src', src).attr('width', width);
+});

14098b24375ba1b1832e0c96ca820d6e83e1522d
David Kinzer <dtkinzer@gmail.com>
2014-07-13 10:07:38 -0400

    Refactor and add facebook dynamic width.

--- js/app.js
+++ js/app.js
@@ -29,18 +29,29 @@ $(function(){
   });
 });
 
+var helios = {};
+helios.get_width = function() {
+  if (matchMedia(Foundation.media_queries.large).matches) {
+    return 500;
+  } else if (matchMedia(Foundation.media_queries.medium).matches) {
+    return 350;
+  }
+  else {
+    return 200;
+  }};
+
 // {{{1 ITunes Modal
 $(function(){
-  var width = (function(){
-    if (matchMedia(Foundation.media_queries.large).matches) {
-      return 500;
-    } else if (matchMedia(Foundation.media_queries.medium).matches) {
-      return 350;
-    }
-    else {
-      return 200;
-    }})();
-
-    var src = "http://widgets.itunes.apple.com/widget.html?c=us&brc=FFFFFF&blc=FFFFFF&trc=FFFFFF&tlc=FFFFFFd=t=&m=music&e=album&w=" + width + "&h=395&ids=535362219&wt=discovery&partnerId=&affiliate_id=";
+  var width = helios.get_width(),
+  src = "http://widgets.itunes.apple.com/widget.html?c=us&brc=FFFFFF&blc=FFFFFF&trc=FFFFFF&tlc=FFFFFFd=t=&m=music&e=album&w=" +
+  width + "&h=395&ids=535362219&wt=discovery&partnerId=&affiliate_id=";
     return $('iframe#itunes').attr('src', src).attr('width', width);
+});
+
+// {{{1 ITunes Modal
+$(function(){
+  var width = helios.get_width(),
+  src = "http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fsoyhelios&width=" +
+  width +  "&height=395&show_faces=false&colorscheme=light&stream=true&border_color&header=false";
+    return $('iframe#facebook').attr('src', src).attr('width', width);
 });
